6e2aa37349895f7b8bc85485741ef41fe83a3bc3
Gabianchini <gabibisdx@gmail.com>
2023-11-10 22:55:53 -0300

    fix:Projects page

--- app/components/pages/projects/page-introduction/index.tsx
+++ app/components/pages/projects/page-introduction/index.tsx
@@ -6,7 +6,7 @@ import { HiArrowNarrowLeft } from "react-icons/hi"
 export const PageIntroduction = () => {
     return(
         <section className="w-full h-[450px] lg:h-[630px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center">
-            <SectionTitle title="Projects" className="text-center items-center [&>h3]:text-4xl"/>
+            <SectionTitle title="Projects"  className="text-center items-center [&>h3]:text-4xl"/>
             <div className="flex flex-col items-center">
                 <p className="text-gray-400 text-center max-w-[640px] my-6 text-md sm:text-base">
                 Here, you'll find a selection of the projects I've worked on. Feel free to browse and delve into the projects to discover how they were created, the technologies employed, and the features implemented.
