49eb3726930e66ba9c375101e7db06ecd00f00ae
Gabianchini <gabibisdx@gmail.com>
2024-04-08 14:37:56 -0300

    Add online link to blog project

--- app/projects/blog/page.tsx
+++ app/projects/blog/page.tsx
@@ -2,14 +2,15 @@
 
 import { ProjectDetails } from "@/app/components/pages/projects/project-details";
 import { TechBadge } from "@/app/components/tech-badge";
-import {FaLink} from "react-icons/fa"
+import {FaLink, FaGithub} from "react-icons/fa"
 import { Button } from "@/app/components/button";
 
 
 export default function Blog() {
     return(
-        <>
-        <ProjectDetails  projectName="Gabe's Blog" techs={<><TechBadge name="Node.js"/> <TechBadge name="MongoDB"/><TechBadge name="Java script"/><TechBadge name="EJS"/><TechBadge name="CSS"/> <TechBadge name="Express"/><TechBadge name="JWT"/> <TechBadge name="Bcrypt"/></>} link="https://github.com/Gabianchini/Nodejs-blog" button={<Button className="min-w-[180px]"><FaLink></FaLink>Code</Button>} description="This is a simple blog project developed as a study project to learn and practice Node.js, Express, and several other technologies.The features are: Users can create an account, sign in, and log out securely. Admins have access to an admin panel where they can add, edit, and delete posts. A about me page dedicated page to provide information about the author or the blog. Posts are displayed with pagination for better user experience."/>
-        </>
+        <ProjectDetails projectName="Gabe's Blog" techs={<><TechBadge name="Node.js"/> <TechBadge name="MongoDB"/><TechBadge name="Java Script"/><TechBadge name="EJS"/><TechBadge name="CSS"/><TechBadge name="Express"/><TechBadge name="JWT"/> <TechBadge name="Bcrypt"/></>} link="https://blog-bs3m.onrender.com/?page=1" button={<Button className="min-w-[180px]" aria-label="Online project"><FaLink></FaLink>Online Project</Button>} description="This is a simple blog project developed as a study project to learn and practice Node.js, Express, and several other technologies.The features are: Users can create an account, sign in, and log out securely. Admins have access to an admin panel where they can add, edit, and delete posts. A about me page dedicated page to provide information about the author or the blog. Posts are displayed with pagination for better user experience.">
+        <a href="https://github.com/Gabianchini/Nodejs-blog" target="_blank">
+        <Button aria-label="Github"><FaGithub></FaGithub>Code</Button></a>
+        </ProjectDetails> 
     )
 }
--- app/projects/hangman/page.tsx
+++ app/projects/hangman/page.tsx
@@ -6,7 +6,7 @@ import {FaLink,FaGithub} from "react-icons/fa"
 import { Button } from "@/app/components/button";
 
 
-export default function Trend() {
+export default function Hangman() {
     return(
         <ProjectDetails projectName="Hangman" techs={<><TechBadge name="Html"/> <TechBadge name="Css"/><TechBadge name="Java Script"/><TechBadge name="API"/></>} link="https://gabianchini.github.io/HangmanGame/" button={<Button className="min-w-[180px]" aria-label="Online project"><FaLink></FaLink>Online Project</Button>} description="The Hangman Game project is a classic word puzzle game that challenges players to guess a hidden word one letter at a time. It's a web-based game created using HTML, JavaScript, and CSS, providing an interactive and entertaining experience for users.">
         <a href="https://github.com/Gabianchini/HangmanGame" target="_blank">
