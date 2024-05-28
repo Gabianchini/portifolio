4f99fd78c2e05c700f75654cabb83786088c4815
Gabianchini <gabibisdx@gmail.com>
2024-05-28 18:49:07 -0300

    Delete online project button

--- app/projects/chat/page.tsx
+++ app/projects/chat/page.tsx
@@ -8,9 +8,7 @@ import { Button } from "@/app/components/button";
 
 export default function Chat() {
     return(
-        <ProjectDetails projectName="Chat" techs={<><TechBadge name="React.js"/><TechBadge name="Java Script"/><TechBadge name="Node.js"/><TechBadge name="MongoDB"/><TechBadge name="Express"/><TechBadge name="Socket.io"/><TechBadge name="TailwindCSS"/><TechBadge name="DaisyUI"/><TechBadge name="Bcrypt"/><TechBadge name="Zustand"/><TechBadge name="Mongoose"/></>} link="https://github.com/Gabianchini/chat-app" button={<Button className="min-w-[180px]"><FaLink></FaLink>Code</Button>}  description="Chat App is a real-time messaging application that allows users to create accounts, sign in, search for other users, and send/receive messages in real-time. It provides a seamless and interactive chatting experience.">
-        <a href="https://github.com/Gabianchini/HangmanGame" target="_blank">
-        <Button aria-label="Github"><FaGithub></FaGithub>Code</Button></a>
+        <ProjectDetails projectName="Chat" techs={<><TechBadge name="React.js"/><TechBadge name="Java Script"/><TechBadge name="Node.js"/><TechBadge name="MongoDB"/><TechBadge name="Express"/><TechBadge name="Socket.io"/><TechBadge name="TailwindCSS"/><TechBadge name="DaisyUI"/><TechBadge name="Bcrypt"/><TechBadge name="Zustand"/><TechBadge name="Mongoose"/></>} link="https://github.com/Gabianchini/chat-app" button={<Button className="min-w-[180px]"><FaGithub></FaGithub>Code</Button>}  description="Chat App is a real-time messaging application that allows users to create accounts, sign in, search for other users, and send/receive messages in real-time. It provides a seamless and interactive chatting experience.">
         </ProjectDetails>
     )
 }
